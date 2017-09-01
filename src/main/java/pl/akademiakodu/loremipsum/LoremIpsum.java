994ae2fdc4d1051b741051e8f83037e3f3d39cde
Lukasz Gebicki <geblukasz@gmail.com>
2017-09-01 15:25:50 +0200

    lorem ipsum html and controller

---
+++ src/main/java/pl/akademiakodu/loremipsum/LoremIpsum.java
@@ -0,0 +1,8 @@
+package pl.akademiakodu.loremipsum;
+
+public interface LoremIpsum {
+
+    String generateParagraphs(int number);
+    String generateSentences(int number);
+    String generateWords(int number);
+}
