b24e44b389a25ea4a8cd01ab28400f4ff6d37045
Lukasz Gebicki <geblukasz@gmail.com>
2017-09-01 13:46:01 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/loremipsum/LoremipsumApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.loremipsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoremipsumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoremipsumApplication.class, args);
+	}
+}
